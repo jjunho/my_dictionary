a88e5651e10174dfde944135b85ec8f9932b8ed4
Juliano Paiva Junho <jjunho@gmail.com>
2025-11-01 14:42:38 +0900

    chore(haskell): add hlint config and hspec test skeleton

--- backend/test/Spec.hs
+++ backend/test/Spec.hs
@@ -1,2 +1,8 @@
+module Main (main) where
+
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ describe "sanity" $ it "true is true" $ True `shouldBe` True
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
